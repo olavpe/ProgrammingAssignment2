cbdb7a0f47709a75af3fb1ae17e98056ef90bfb6
olavpe <olavpe@gmail.com>
2016-10-13 17:05:13 +0200

    Programming Assignment Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function takes a matrix as input, calculates inverse and produces 
+## a list containing both the original matrix and the inverse matrix. 
+## The second function takes the produced list and will either produced the
+## cached inverse if already calcuated and if it is not present will
+## calculate it.
 
-## Write a short comment describing this function
+## This function takes in a matrix, calculates its inverse, and outputs both 
+## the original and inverse matrix as functions in a list. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    CachedInverse <- solve(x)
+    inverse <- function () CachedInverse
+    matrix <- function () x
+    list(matrix = matrix, inverse = inverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes the a list and if present, will retrieve the inverse
+## matrix cached in the list. If it is not present it will calculate the
+## inverse matrix from the original matrix present in the list.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$inverse()
+    if(!is.null(inv)){
+        message("Retrieving cached inverse")
+        return(inv)
+    }
+    else{inv <- solve(x$matrix())}
+    inv
 }
