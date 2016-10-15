cc23fefa179a9439982d0fb3325e5a6f64ff0236
olavpe <olavpe@gmail.com>
2016-10-15 14:08:36 +0200

    R programming Week 3 - Peer Reviewed Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,39 @@
 ## The first function takes a matrix as input, calculates inverse and produces 
-## a list containing both the original matrix and the inverse matrix. 
-## The second function takes the produced list and will either produced the
-## cached inverse if already calcuated and if it is not present will
-## calculate it.
+## a list containing functions that will; set, get, set the inverse, get the
+## inverse matrix. The second function takes the produced list and will
+## produce the cached inverse matrix.
 
-## This function takes in a matrix, calculates its inverse, and outputs both 
-## the original and inverse matrix as functions in a list. 
 
-makeCacheMatrix <- function(x = matrix()) {
-    CachedInverse <- solve(x)
-    inverse <- function () CachedInverse
-    matrix <- function () x
-    list(matrix = matrix, inverse = inverse)
+## This function takes in a matrix and outputs a list of functions to be 
+## used to solve the inverse matrix
+
+makeCacheMatrix <- function(x = numeric()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## This function takes the a list and if present, will retrieve the inverse
-## matrix cached in the list. If it is not present it will calculate the
-## inverse matrix from the original matrix present in the list.
+## This function takes the a list. If the matrix has already been calculated
+## it will just retrieve the cached data, but if not it will execute the
+## functions that calculate the inverse matrix.
 
 cacheSolve <- function(x, ...) {
-    inv <- x$inverse()
-    if(!is.null(inv)){
-        message("Retrieving cached inverse")
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("Retrieving Cached Data")
         return(inv)
     }
-    else{inv <- solve(x$matrix())}
+    matrix <- x$get()
+    inv <- solve(matrix, ...)
+    x$setinv(inv)
     inv
 }
